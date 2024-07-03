dcf978de109d6c1ffb7e1d59af00a4506520d4aa
Nate Lee <nleepercussion@gmail.com>
2024-07-03 12:26:45 -0400

    add hot reload

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@
 
 const functions = require('@google-cloud/functions-framework');
 
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3333;
-
 const cors = require('cors');
 
 const express = require('express');
@@ -15,12 +11,6 @@ app.use(cors());
 app.use(express.static('public'))
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-
-// const server = http.createServer(app);
-
-// server.listen(port, hostname, () => {
-//     console.log(`Server is running at ${hostname}:${port}`)
-// });
 
 const rootController = require('./routes/index');
 const itemController = require('./routes/items');
